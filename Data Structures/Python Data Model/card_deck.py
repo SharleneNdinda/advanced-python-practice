e3f67c672b7b68b0ada4ba36170978577f350c2c
sharlene.mutuku <sharlene.mutuku@savannahinformatics.com>
2024-09-03 16:57:10 +0300

    feat: Sort Card Deck

--- Data Structures/Python Data Model/card_deck.py
+++ Data Structures/Python Data Model/card_deck.py
@@ -2,6 +2,8 @@ import collections
 
 Card = collections.namedtuple("Card", ["rank", "suit"])
 
+card = Card("7", "hearts")
+print(len(card))
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list("JQKA")
@@ -16,11 +18,23 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
-card = Card("7", "hearts")
-print(len(card))
 
 deck = FrenchDeck()
 print(len(deck)) # returns 52
 
 # __getitem__ also supports slicing
-print(deck[:3])  
+print(deck[:3])
+
+# __getitem__ also supports iteration
+for card in deck:
+    print(card)
+
+# sorting cards in order of increasing rank
+suits_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suits_values) + suits_values[card.suit]
+
+for card in sorted(deck, key=spades_high):
+    print(card)
